49ce76d204916c8fe0add01b87a54b4e36d1d38c
Marcelo Mogrovejo <mmogrovejo@mmogrovejo.local>
2017-05-31 11:54:28 -0300

    fist commit

---
+++ Movie/Movie/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by Marcelo Mogrovejo on 5/31/17.
+//  Copyright © 2017 Marcelo Mogrovejo. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    var coreData = CoreDataStack()
+    var managedObjectContext: NSManagedObjectContext!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let movie = Movie(context: managedObjectContext)
+//        let name = movie.title
+        
+        
+        let movie = Movie(context: coreData.persistentContainer.viewContext)
+        let movies = movie.getMovieInfo(title: "X-Men", context: coreData.persistentContainer.viewContext)
+        
+        if movies != nil {
+            print(movie.format!)
+        } else {
+            print("No movie info is found")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
